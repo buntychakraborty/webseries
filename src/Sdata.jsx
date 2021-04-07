0a6259eb5afe9caa101544f99bcb4477449dd086
buntychakraborty <buntychakraborty@outlook.com>
2021-04-07 08:57:32 +0530

    initial commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,35 @@
+const Sdata = [
+    {
+        imgsrc:"https://origin-staticv2.sonyliv.com/landscape_thumb/Scam1992_Landscape_Thumb_Review.jpg",
+        title:"Sony Liv Original Series",
+        sname:"Scam 1992",
+        link:"https://www.sonyliv.com/shows/scam-1992-the-harshad-mehta-story-1700000292"
+    },
+    {
+        imgsrc:"https://m.media-amazon.com/images/M/MV5BZWJiMDZmNTAtZDg5YS00NDgyLThkMzctYTczZjllNzU0YWY5XkEyXkFqcGdeQXVyMTAwMDI3ODEw._V1_.jpg",
+        title:"Voot Original Series",
+        sname:"Asur",
+        link:"https://www.voot.com/shows/asur/121993?utm_source=google_web&utm_medium=watchaction"
+    },
+    {
+        imgsrc:"https://wallpapercave.com/wp/wp4025667.jpg",
+        title:"A Netflix Original Series",
+        sname:"Sacred Games",
+        link:"https://www.netflix.com/in/title/80115328?source=35"
+    }
+    ,
+    {
+        imgsrc:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTdQDtNp_-o-GGCuN_pnzGnqDwSkWX8CCzTL5wsyT1zoI8X2YeW",
+        title:"A Netflix Original Series",
+        sname:"A Suitable Boy",
+        link:"https://www.netflix.com/in/title/81308413?source=35"
+    },
+    {
+        imgsrc:"https://upload.wikimedia.org/wikipedia/en/1/1a/Panchayat_logo.jpg",
+        title:"Prime Original Series",
+        sname:"Panchayat",
+        link:"https://www.primevideo.com/dp/amzn1.dv.gti.50b85fc5-aff5-347c-4a41-2233d3e1983a?autoplay=1&ref_=atv_cf_strg_wb"
+    }
+];
+
+export default Sdata;
